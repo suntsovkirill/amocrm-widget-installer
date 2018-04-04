f6f8fe3efe9fc9a4304a9f7797a2d85cfa143053
Suntsov Kirill <suntsov.kirill@gmail.com>
2018-04-04 19:38:32 +0500

    Replace axios to request

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 const fs = require('fs');
 const path = require('path');
-const querystring = require('querystring');
-const axios = require('axios');
 const archiver = require('archiver');
 const argparse = require('argparse');
+const request = require('request-promise');
+const contentType = require('content-type');
 
 
 class WidgetInstaller {
@@ -15,31 +15,27 @@ class WidgetInstaller {
         this.domain = domain;
         this.needUpdate = needUpdate;
 
-        this._request = axios.create({
-            baseURL: `https://${this.domain}.amocrm.ru/`,
-            timeout: 60000,
+        this._request = request.defaults({
             headers: {
                 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0',
                 'X-Requested-With': 'XMLHttpRequest'
             },
-            validateStatus(status) {
-                return (status === 200 || status === 204);
-            }
-        });
-
-        this._request.interceptors.response.use(response => {
-            if (response && typeof response.headers === 'object' && 'set-cookie' in response.headers) {
-                if (!('Cookie' in this._request.defaults.headers.common)) {
-                    this._request.defaults.headers.common['Cookie'] = response.headers['set-cookie'].join('; ');
+            jar: true,
+            transform(body, response) {
+                const ct = contentType.parse(response.headers['content-type']);
+                if (ct.type === 'application/json' || ct.type === 'text/json') {
+                    return JSON.parse(body);
                 }
+
+                return body;
             }
-            return response;
         });
     }
 
     auth() {
-        return this._request.get('/private/api/auth.php?type=json', {
-            data: {
+        return this._request.get(`https://${this.domain}.amocrm.ru/private/api/auth.php`, {
+            qs: {
+                'type': 'json',
                 'USER_LOGIN': this.login,
                 'USER_HASH': this.api_key
             }
@@ -47,14 +43,12 @@ class WidgetInstaller {
     }
 
     async createWidgetKey(code) {
-        const result = await this._request.post('/ajax/settings/dev/', querystring.stringify({action: 'create', code}), {
-            headers: {
-                "Content-Type" : "application/x-www-form-urlencoded; charset=UTF-8"
-            }
+        const result = await this._request.post(`https://${this.domain}.amocrm.ru/ajax/settings/dev/`, {
+            formData: {action: 'create', code}
         });
 
-        if ('error' in result.data) {
-            throw new Error(result.data.error);
+        if ('error' in result) {
+            throw new Error(result.error);
         } else {
             console.log(`Coздан виджет с кодом: ${code}`);
             return await this.findWidgetData(code);
@@ -62,16 +56,16 @@ class WidgetInstaller {
     }
 
     async findWidgetData(code) {
-        const result = await this._request.get('/ajax/settings/dev/');
+        const result = await this._request.get(`https://${this.domain}.amocrm.ru/ajax/settings/dev/`);
 
-        if ('error' in result.data) {
-            throw new Error(result.data.error);
+        if ('error' in result) {
+            throw new Error(result.error);
         } else {
-            const items = result.data['response']['widgets']['items'];
+            const items = result.response.widgets.items;
 
             for (let item of items) {
                 if (typeof item === 'object' && item.hasOwnProperty('code') && item.code === code) {
-                    console.log(`Найден виджет с кодом ${item['code']} и секретным ключом ${item['secret_key']}.`);
+                    console.log(`Найден виджет с кодом ${item.code} и секретным ключом ${item.secret_key}.`);
                     return [item['code'], item['secret_key']];
                 }
             }
@@ -81,40 +75,23 @@ class WidgetInstaller {
 
     async uploadWidget(archivePath, secretKey, widgetCode) {
         const result = await this._request.post(`https://widgets.amocrm.ru/${this.domain}/upload/`, {
-            ' widget': fs.readFileSync(archivePath),
-            'secret': secretKey,
-            'widget': widgetCode,
-            'amouser': this.login,
-            'amohash': this.api_key,
-            'domain': `amocrm.ru`
-        }, {
-            transformRequest: [(data, headers) => {
-                const boundary = '-----------------------------' + Date.now();
-                headers['Content-Type'] = `multipart/form-data; boundary=${boundary}`;
-
-                const tohex = (value) => Buffer.from(value).toString('hex');
-
-                let result = tohex('\r\n\r\n');
-
-                for (let key in data) {
-                    result += tohex(`--${boundary}\r\n`);
-                    if (key === ' widget') {
-                        result += tohex('Content-Disposition: form-data; name="widget"; filename="widget.zip"\r\nContent-Type: application/x-zip-compressed\r\n\r\n');
-                        result += tohex(data[key]);
-                        result += tohex('\r\n');
-                    } else {
-                        result += tohex(`Content-Disposition: form-data; name="${key}"\r\n\r\n`);
-                        result += tohex(data[key] + '\r\n');
+            formData: {
+                ' widget': {
+                    value: fs.createReadStream(archivePath),
+                    options: {
+                        filename: 'widget.zip',
+                        contentType: 'application/x-zip-compressed'
                     }
-                }
-
-                result += tohex(`--${boundary}--\r\n`);
-
-                return Buffer.from(result, 'hex');
-            }],
+                },
+                'secret': secretKey,
+                'widget': widgetCode,
+                'amouser': this.login,
+                'amohash': this.api_key,
+                'domain': `amocrm.ru`
+            }
         });
 
-        const text = JSON.parse(result.data.replace(/^<script(.*)script>/, ''));
+        const text = JSON.parse(result.replace(/^<script(.*)script>/, ''));
 
         if ('error' in text) {
             throw new Error(text.error)
@@ -151,8 +128,8 @@ class WidgetInstaller {
         const manifestPath = path.resolve(widgetFolder, 'manifest.json');
         const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf8'));
 
-        manifest['widget']['code'] = code;
-        manifest['widget']['secret_key'] = secret_key;
+        manifest.widget.code = code;
+        manifest.widget.secret_key = secret_key;
 
         fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
         console.log('Файл manifest.json изменен.');
@@ -183,19 +160,18 @@ class WidgetInstaller {
         const wi = new WidgetInstaller(args.subdomain, args.login, args.apikey, args.update);
         await wi.auth();
 
-        let [code, secret_key] = [...await wi.findWidgetData(args.name)];
+        let [code, secretKey] = [...await wi.findWidgetData(args.name)];
 
         if (!code) {
-            [code, secret_key] = [...await wi.createWidgetKey(args.name)];
+            [code, secretKey] = [...await wi.createWidgetKey(args.name)];
         } else if (!args.update) {
-            console.log(`Виджет с кодом ${code} уже существует, для его обновления укажите параметр --update`);
-            return;
+            return console.log(`Виджет с кодом ${code} уже существует, для его обновления укажите параметр --update`);
         }
 
-        WidgetInstaller.updateManifest(args.folder, secret_key, code);
+        WidgetInstaller.updateManifest(args.folder, secretKey, code);
 
         const archivePath = await WidgetInstaller.zipArchive(args.folder);
-        await wi.uploadWidget(archivePath, secret_key, code);
+        await wi.uploadWidget(archivePath, secretKey, code);
     } catch (e) {
         console.error(e.message);
     }
